74dc6034ffac9c759637b59e7f65aa1b3c9b00a5
Om Ji Dubey <omdubey7001.hash@gmail.com>
2026-01-02 13:55:31 +0530

    Util code discussion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 
 
 
-connectDB() 
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000),() =>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    }
+})
+.catch((err) =>{
+    console.error(`Mongo DB connection failed !!`,err)
+})
 
 
 
